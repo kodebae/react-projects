564cf1c9f73be4bace04b96fad366baaa227622f
Karmen Durbin <karmendurbin@Karmens-MacBook-Air.local>
2021-03-14 15:08:23 -0400

    Finished birthday reminder app, src file ready to be hosted

--- 1-birthday-reminder/setup/README.md
+++ 1-birthday-reminder/setup/README.md
@@ -1,3 +1,4 @@
 ## Idea
-
+### Birthday Reminder
+This app makes use of React useState and renders static data to the DOM. Styling can be altered in the index.css file. 
 [https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import data from './data'; /*import our static data*/
+import List from './List'; /*imports the list component*/
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+  <main>
+    <section className="container">
+      <h3>{people.length} birthday's today 🎂</h3>
+      <List people={people} />
+      <button onClick={() => setPeople([])}>
+        clear all
+      </button>
+    </section>
+  </main>
+  );
 }
 
 export default App;
+/*exporting the default app, this is our main func that displays components, imports data, and passes state around. We have HTML structure and our main*/
